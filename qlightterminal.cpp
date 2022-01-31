ffb77a41919850c99753b22f7b97417099c53aa4
Plesker <florian.plesker@web.de>
2022-01-31 23:03:39 +0100

    Added basic special key handling.

--- qlightterminal.cpp
+++ qlightterminal.cpp
@@ -12,6 +12,7 @@
 #include <QLayout>
 #include <QGraphicsAnchorLayout>
 #include <QPoint>
+#include <QKeyCombination>
 
 #if DEBUGGING
     #include <QDebug>
@@ -167,13 +168,122 @@ void QLightTerminal::paintEvent(QPaintEvent *event){
 void QLightTerminal::keyPressEvent(QKeyEvent *e){
     QString input = e->text();
 
-    QByteArray text;
-    if(input.contains('\n')){
-        text = input.left(input.indexOf('\n')+1).toUtf8();
+    // normal input
+    if(input != ""){
+        QByteArray text;
+        if(input.contains('\n')){
+            text = input.left(input.indexOf('\n')+1).toUtf8();
+        } else {
+            text = e->text().toUtf8();
+        }
+        st->ttywrite(text, text.size(),1);
     } else {
-        text = e->text().toUtf8();
+        // special keys
+        // TODO: Add more short cuts
+        // TODO: Refactor the if statements to dynamically add the escape sequence and modifier
+        switch(e->key()){
+        case Qt::Key_Up:
+            if(mods == Qt::NoModifier){
+                st->ttywrite("\033[A", 3,1);
+            } else if(mods == Qt::ShiftModifier){
+                st->ttywrite("\033[1;2A",5,1);
+            } else if(mods == Qt::AltModifier){
+                st->ttywrite("\033[1;3A",5,1);
+            } else if(mods == Qt::ControlModifier){
+                st->ttywrite("\033[1;5A",5,1);
+            } else {
+                st->ttywrite("\033[A", 3,1);
+            }
+            break;
+        case Qt::Key_Down:
+            if(mods == Qt::NoModifier){
+                st->ttywrite("\033[B", 3,1);
+            } else if(mods == Qt::ShiftModifier){
+                st->ttywrite("\033[1;2B",5,1);
+            } else if(mods == Qt::AltModifier){
+                st->ttywrite("\033[1;3B",5,1);
+            } else if(mods == Qt::ControlModifier){
+                st->ttywrite("\033[1;5B",5,1);
+            } else {
+                st->ttywrite("\033[B", 3,1);
+            }
+            break;
+        case Qt::Key_Left:
+            if(mods == Qt::NoModifier){
+                st->ttywrite("\033[D", 3,1);
+            } else if(mods == Qt::ShiftModifier){
+                st->ttywrite("\033[1;2D",5,1);
+            } else if(mods == Qt::AltModifier){
+                st->ttywrite("\033[1;3D",5,1);
+            } else if(mods == Qt::ControlModifier){
+                st->ttywrite("\033[1;5D",5,1);
+            } else {
+                st->ttywrite("\033[D", 3,1);
+            }
+            break;
+        case Qt::Key_Right:
+            if(mods == Qt::NoModifier){
+                st->ttywrite("\033[C", 3,1);
+            } else if(mods == Qt::ShiftModifier){
+                st->ttywrite("\033[1;2C",5,1);
+            } else if(mods == Qt::AltModifier){
+                st->ttywrite("\033[1;3C",5,1);
+            } else if(mods == Qt::ControlModifier){
+                st->ttywrite("\033[1;5C",5,1);
+            } else {
+                st->ttywrite("\033[C", 3,1);
+            }
+            break;
+        case Qt::Key_F1:
+            if(mods == Qt::NoModifier){
+                st->ttywrite("\033OP",3,1);
+            }
+            break;
+        case Qt::Key_F2:
+            if(mods == Qt::NoModifier){
+                st->ttywrite("\033OQ",3,1);
+            }
+            break;
+        case Qt::Key_F3:
+            if(mods == Qt::NoModifier){
+                st->ttywrite("\033OR",3,1);
+            }
+            break;
+        case Qt::Key_F4:
+            if(mods == Qt::NoModifier){
+                st->ttywrite("\033OS",3,1);
+            }
+            break;
+        case Qt::Key_F5:
+            if(mods == Qt::NoModifier){
+                st->ttywrite("\033[15~",6,1);
+            }
+            break;
+        case Qt::Key_F6:
+            if(mods == Qt::NoModifier){
+                st->ttywrite("\033[17~",6,1);
+            }
+            break;
+        case Qt::Key_F7:
+            if(mods == Qt::NoModifier){
+                st->ttywrite("\033[18~",6,1);
+            }
+            break;
+        case Qt::Key_F8:
+            if(mods == Qt::NoModifier){
+                st->ttywrite("\033[19~",6,1);
+            }
+            break;
+        case Qt::Key_F9:
+            if(mods == Qt::NoModifier){
+                st->ttywrite("\033[20~",6,1);
+            }
+            break;
+        default:
+            // do nothing;
+            break;
+        }
     }
-    st->ttywrite(text, text.size(),1);
 }
 
 void QLightTerminal::mousePressEvent(QMouseEvent *event){
@@ -210,7 +320,7 @@ void QLightTerminal::wheelEvent(QWheelEvent *event)
 
     if (!numPixels.isNull()) {
         // TODO: test if this works << needs mouse with finer resolution
-        scrollbar.setValue(scrollbar.value() + numPixels.y()*15);
+        scrollbar.setValue(scrollbar.value() - numPixels.y()*1.5);
         scrollX(scrollbar.value());
         return;
     }
